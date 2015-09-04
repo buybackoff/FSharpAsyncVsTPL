d437bb32e5df4b88f51ec21b715e52ea9ec72690
buybackoff <vbaybekov@gmail.com>
2015-09-04 04:12:17 +0300

    Test F# computation expressions vs C# async/await

---
+++ CSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CSharp {
+    class Program {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var sum = 0;
+            var t = Task.Run(async () =>
+            {
+                while (sum < 10000000)
+                {
+                    var r = await Task.FromResult(1);  //await Task.Run(() => 1);// Task.FromResult(1);
+                    sum += r;
+                }
+                
+                return sum;
+            });
+            t.Wait();
+            sw.Stop();
+            Console.WriteLine("Sum:" + t.Result);
+            Console.WriteLine("Elapsed:" + sw.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FSharpTasks/Program.fs
@@ -0,0 +1,63 @@
+﻿open System
+open System.Diagnostics
+open System.Threading
+open System.Threading.Tasks
+open Test
+
+[<EntryPoint>]
+let main argv = 
+  let sw = Stopwatch()
+  sw.Start()
+  let mutable sum = 0
+  //for i in 0..1000000 do
+  let t = 
+    let rec loop () =
+      task {
+        let task = Task.FromResult(1)
+        if task.IsCompleted then
+          sum <- sum + task.Result
+          if sum < 10000000 then
+            return! loop ()
+          else return sum
+        else
+          let! result = task //Task.FromResult(1)
+          sum <- sum + result
+          if sum < 10000000 then
+            return! loop ()
+          else return sum
+      }
+    loop ()
+  ()
+//  let t = 
+//    async {
+//      let mutable sum = 0
+//      for i in 0..1000000 do
+//        let! result = Task.FromResult(1) |> Async.AwaitTask
+//        sum <- sum + result
+//      return sum
+//    } |> Async.StartAsTask
+
+//  let t = 
+//    let mutable sum = 0
+//    for i in 0..1000000 do
+//      let result = Task.FromResult(1).Result
+//      sum <- sum + result
+//    Task.FromResult(sum)
+//
+//  let t = 
+//    let mutable sum = 0
+//    for i in 0..1000000 do
+//      let result = Task.Run(fun _ -> 1).Result
+//      sum <- sum + result
+//    Task.FromResult(sum)
+
+  let r = t.Result
+  sw.Stop()
+  Console.WriteLine("Elapsed : " + sw.ElapsedMilliseconds.ToString())
+  Console.WriteLine("Sum : " + r.ToString())
+  Console.ReadLine() |> ignore
+  ()
+  
+
+  printfn "%A" argv
+  0 // return an integer exit code
